e9d2785ede4920aee5da2364e38a12105afa9121
Arpan Das <arp14@yahoo.com>
2018-08-21 09:26:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Wipro TalentNext PBL
+
+Topics Covered String based programs (part-2)
+
+Identify possible words
+https://tests.mettl.com/authenticateKey/13486c16
+
+Encoding Three Strings
+https://tests.mettl.com/authenticateKey/f05028d5
---
+++ src/com/w3epic/wiprotraining/IdentifyPossibleWords.java
@@ -0,0 +1,30 @@
+package com.w3epic.wiprotraining;
+
+import java.io.*;
+import  java.util.*;
+
+// Read only region start
+class IdentifyPossibleWords {
+
+	public String identifyPossibleWords(String input1,String input2){
+		// Read only region end
+		input1 = input1.toUpperCase();
+		
+		StringBuffer output = new StringBuffer();
+		String[] words = input2.split(":");
+		
+		int underscoreIndex = input1.indexOf('_');
+		
+		for (int i = 0; i < words.length; i++) {
+			words[i] = words[i].toUpperCase();
+
+			if (words[i].length() >= input1.length() &&
+			input1.replace('_', words[i].charAt(underscoreIndex)).equals(words[i])) {
+				output.append(words[i]).append(":");
+			}
+		}
+		
+		if (output.length() == 0) return "ERROR-009";
+        else return output.toString().substring(0, output.length() - 1);
+	}
+}
